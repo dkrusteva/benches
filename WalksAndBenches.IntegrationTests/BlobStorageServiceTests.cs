efb5e323b8d28fa9b0eb0ba66bc9bc958eed418a
hduniverse <30510470+hduniverse@users.noreply.github.com>
2019-10-13 21:41:39 +0100

    Add tests for BlobStorageService

--- WalksAndBenches.IntegrationTests/BlobStorageServiceTests.cs
+++ WalksAndBenches.IntegrationTests/BlobStorageServiceTests.cs
@@ -4,9 +4,11 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using NUnit.Framework;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WalksAndBenches.IntegrationTests;
+using WalksAndBenches.Models;
 using WalksAndBenches.Services;
 
 namespace WalksAndBenches.IntegrationTests
@@ -25,7 +27,7 @@ namespace WalksAndBenches.IntegrationTests
         {
             _factory = new BenchesWebApplicationFactory();
             _client = _factory.CreateClient();
-            _containerName = Guid.NewGuid().ToString().ToLower();
+            _containerName = $"Container_{Guid.NewGuid().ToString().ToLower()}";
 
             AzureStorageConfig azureAppSettings = new AzureStorageConfig()
             {
@@ -59,6 +61,84 @@ namespace WalksAndBenches.IntegrationTests
             Assert.IsTrue(permissions.PublicAccess.Equals(BlobContainerPublicAccessType.Blob));
         }
 
+        [Test]
+        public async Task GetBlobs_WithContainerName_GetsBlobsFromContainer()
+        {
+            // Arrange
+            await _sut.ConfigureBlobStorage();
+            var model = new WalkModel()
+            {
+                WalkName = "Test walk",
+                Description = "A walk in the test realm",
+                SubmittedBy = "Test user"
+            };
+            var otherModel = new WalkModel()
+            {
+                WalkName = "Other test walk",
+                Description = "A walk in the other test realm",
+                SubmittedBy = "Other test user"
+            };
+            using (var stream = GenerateStreamFromString("a,b \n c,d"))
+            {
+                await _sut.Save(stream, model);
+            }
+            using (var stream = GenerateStreamFromString("e,f \n g,h"))
+            {
+                await _sut.Save(stream, otherModel);
+            }
+
+            // Act
+            var blobs = await _sut.GetBlobs();
+
+            // Assert
+            Assert.AreEqual(2, blobs.Count);
+            var text = await blobs[0].DownloadTextAsync();
+            Assert.AreEqual("e,f \n g,h", text);
+            var otherText = await blobs[1].DownloadTextAsync();
+            Assert.AreEqual("a,b \n c,d", otherText);
+            await AssertMetadata(blobs[1], "Other test walk", "A walk in the other test realm", "Other test user");
+            await AssertMetadata(blobs[1], "Test walk", "A walk in the test realm", "Test user");
+        }
+
+        [Test]
+        public async Task Save_WithFileAndModel_SavesBlobToContainer()
+        {
+            // Arrange
+            await _sut.ConfigureBlobStorage();
+            var model = new WalkModel()
+            {
+                WalkName = "Test walk",
+                Description = "A walk in the test realm",
+                SubmittedBy = "Test user"
+            };
+
+            // Act
+            using (var stream = GenerateStreamFromString("a,b \n c,d"))
+            {
+                await _sut.Save(stream, model);
+            }
+
+            // Assert
+            var blob = GetContainerReference(_containerName)
+                .GetBlockBlobReference("Test walk");
+            Assert.IsTrue(await blob.ExistsAsync());
+            var text = await blob.DownloadTextAsync();
+            Assert.AreEqual("a,b \n c,d", text);
+            await AssertMetadata(blob, "Test walk", "A walk in the test realm", "Test user");
+        }
+
+        private async Task AssertMetadata(CloudBlockBlob blob, string name, string description, string submittedBy)
+        {
+            await blob.FetchAttributesAsync();
+            blob.Metadata.TryGetValue("description", out var d);
+            blob.Metadata.TryGetValue("walkname", out var n);
+            blob.Metadata.TryGetValue("submittedby", out var s);
+
+            Assert.AreEqual("Test walk", n);
+            Assert.AreEqual("A walk in the test realm", d);
+            Assert.AreEqual("Test user", s);
+        }
+
         private CloudBlobContainer GetContainerReference(string containerName)
         {
             return CloudStorageAccount.Parse(_connectionString)
@@ -66,8 +146,25 @@ namespace WalksAndBenches.IntegrationTests
                 .GetContainerReference(containerName);
         }
 
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var memoryStream = new MemoryStream();
+            var writer = new StreamWriter(memoryStream);
+            writer.Write(s);
+            writer.Flush();
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            var container = GetContainerReference(_containerName);
+            await container.DeleteIfExistsAsync();
+        }
+
         [OneTimeTearDown]
-        public void TearDown()
+        public void OneTimeTearDown()
         {
             _client.Dispose();
             _factory.Dispose();
